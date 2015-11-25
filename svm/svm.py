a2c8e57b6bc299d583b5802f2d4abdd6e4ccfcfd
snavien@bitbucket.org <snavien@bitbucket.org>
2015-11-24 17:43:51 -0800

    testing sparkcontext

--- svm/svm.py
+++ svm/svm.py
@@ -1,22 +1,25 @@
 from pyspark.mllib.classification import SVMWithSGD, SVMModel
 from pyspark.mllib.regression import LabeledPoint
+from pyspark import SparkContext
 
-# Load and parse the data
-def parsePoint(line):
-    values = [float(x) for x in line.split(' ')]
-    return LabeledPoint(values[0], values[1:])
 
-data = sc.textFile("dataLibSVM.txt")
-parsedData = data.map(parsePoint)
-
-# Build the model
-model = SVMWithSGD.train(parsedData, iterations=100)
-
-# Evaluating the model on training data
-labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
-trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
-print("Training Error = " + str(trainErr))
-
-# Save and load model
-model.save(sc, "myModelPath")
-sameModel = SVMModel.load(sc, "myModelPath")
+sc = SparkContext("local", "SVM app")
+# # Load and parse the data
+# def parsePoint(line):
+#     values = [float(x) for x in line.split(' ')]
+#     return LabeledPoint(values[0], values[1:])
+#
+# data = sc.textFile("dataLibSVM.txt")
+# parsedData = data.map(parsePoint)
+#
+# # Build the model
+# model = SVMWithSGD.train(parsedData, iterations=100)
+#
+# # Evaluating the model on training data
+# labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
+# trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
+# print("Training Error = " + str(trainErr))
+#
+# # Save and load model
+# model.save(sc, "myModelPath")
+# sameModel = SVMModel.load(sc, "myModelPath")
