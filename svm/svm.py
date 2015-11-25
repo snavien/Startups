4af3f577177a753c968f5b6e2b6d4ddac50430b2
snavien@bitbucket.org <snavien@bitbucket.org>
2015-11-24 23:52:10 -0800

    finished svm

--- svm/svm.py
+++ svm/svm.py
@@ -1,25 +1,40 @@
 from pyspark.mllib.classification import SVMWithSGD, SVMModel
 from pyspark.mllib.regression import LabeledPoint
-from pyspark import SparkContext
+from pyspark import SparkConf, SparkContext
+from numpy.random import randint
+
+#sc = SparkContext("local", "SVM App", pyFiles=['dataLibSVM.txt'])
+conf = (SparkConf()
+        .setMaster("local")
+        .setAppName("SVM App")
+        .set("spark.executor.memory", "500m"))
+sc = SparkContext(conf=conf)
+# Load and parse the data
+def parsePoint(line):
+    values = [float(x) for x in line.split(',')]
+    # print(values[0])
+    return LabeledPoint(values[0], values[2:])
 
 
-sc = SparkContext("local", "SVM app")
-# # Load and parse the data
-# def parsePoint(line):
-#     values = [float(x) for x in line.split(' ')]
-#     return LabeledPoint(values[0], values[1:])
-#
-# data = sc.textFile("dataLibSVM.txt")
-# parsedData = data.map(parsePoint)
-#
-# # Build the model
-# model = SVMWithSGD.train(parsedData, iterations=100)
-#
-# # Evaluating the model on training data
-# labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
-# trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
-# print("Training Error = " + str(trainErr))
-#
-# # Save and load model
-# model.save(sc, "myModelPath")
-# sameModel = SVMModel.load(sc, "myModelPath")
+data = sc.textFile("numData.csv")
+parsedData = data.map(parsePoint)
+
+
+def run_iterations(parsedData, iter, seed):
+    trainingData, testingData = parsedData.randomSplit([70, 30], seed)
+    print("For " + str(iter) + " iterations:")
+    # Build the model
+    model = SVMWithSGD.train(trainingData, iterations=100)
+
+    # Evaluating the model on training data
+    labelsAndPreds = trainingData.map(lambda p: (p.label, model.predict(p.features)))
+    trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(trainingData.count())
+    print("Training Error = " + str(trainErr))
+
+    labelsAndPreds = testingData.map(lambda p: (p.label, model.predict(p.features)))
+    testErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(testingData.count())
+
+    print("Testing Error = " + str(testErr))
+
+for iter in [100, 1000, 10000]:
+    run_iterations(parsedData, iter, randint(1, 9999))
